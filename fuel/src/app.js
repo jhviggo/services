9688d1516a88fbff1bbc5d5194257b2bff6794d9
Viggo Petersen <vig56@hotmail.com>
2023-08-30 23:43:47 +0200

    get ci to work

--- fuel/src/app.js
+++ fuel/src/app.js
@@ -1,12 +1,12 @@
-import express from 'express';
+const express = require('express');
 
-const app = express()
-const port = 80
+const app = express();
+const port = 80;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  res.send('I am the goodest fuel service! frick yea!');
+});
 
 app.listen(port, () => {
-  console.log(`App running on port ${port}`)
-})
+  console.log(`App running on port ${port}`);
+});
